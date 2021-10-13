ac2b38277d6a29040cc3f3973210500fe03b3ebe
Antonio Joao Zimila <antoniojoaozimila@gmail.com>
2021-10-13 14:46:26 -0300

    adicionando JFRAME

---
+++ build/generated-sources/ap-source-output/java_beans/Aluno_.java
@@ -0,0 +1,22 @@
+package java_beans;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Aluno.class)
+public abstract class Aluno_ {
+
+	public static volatile SingularAttribute<Aluno, Double> preco;
+	public static volatile SingularAttribute<Aluno, String> aluno_classe;
+	public static volatile SingularAttribute<Aluno, Integer> Id;
+	public static volatile SingularAttribute<Aluno, String> aluno_nome;
+
+	public static final String PRECO = "preco";
+	public static final String ALUNO_CLASSE = "aluno_classe";
+	public static final String ID = "Id";
+	public static final String ALUNO_NOME = "aluno_nome";
+
+}
+
---
+++ build/generated-sources/ap-source-output/java_beans/Classe_Aluno_.java
@@ -0,0 +1,20 @@
+package java_beans;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Classe_Aluno.class)
+public abstract class Classe_Aluno_ {
+
+	public static volatile SingularAttribute<Classe_Aluno, Double> preco;
+	public static volatile SingularAttribute<Classe_Aluno, Integer> Id;
+	public static volatile SingularAttribute<Classe_Aluno, String> classe_nome;
+
+	public static final String PRECO = "preco";
+	public static final String ID = "Id";
+	public static final String CLASSE_NOME = "classe_nome";
+
+}
+
